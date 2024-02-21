f33dbc158adfd243dcf55cf14002b8352607bf54
EinfachMichi <94147049+EinfachMichi@users.noreply.github.com>
2024-02-21 10:01:30 +0100

    Name

--- main.cpp
+++ main.cpp
@@ -3,64 +3,76 @@ import scara_functions;
 #include <iostream>
 #include <random>
 
+class Name
+{
+public:
+    Name() { memory_ = new char[1] { 0 }; }
+    explicit Name(const char* name)
+    {
+        length_ = static_cast<int>(strlen(name)) + 1;
+        memory_ = new char[length_];
+        strcpy_s(memory_, length_, name);
+    }
+
+    Name(const Name& name)
+    {
+        memory_ = new char[name.length_];
+        length_ = name.length_;
+        strcpy_s(memory_, length_, name.memory_);
+    }
+    
+    ~Name()
+    {
+        delete[] memory_;
+    }
+
+    [[nodiscard]] const char* get_name() const
+    {
+        return memory_;
+    }
+
+    [[nodiscard]] int get_length() const
+    {
+        return length_;
+    }
+
+    Name& operator=(const Name& name)
+    {
+        if(name.memory_ != memory_)
+        {
+            delete[] memory_;
+            memory_ = new char[name.length_];
+            length_ = name.length_;
+            strcpy_s(memory_, length_, name.memory_);
+        }
+        return *this;
+    }
+
+private:
+    char* memory_ = nullptr;
+    int length_ = 0; 
+};
+
 int main()
 {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(0,2000000000);
-    
-    start("AbsoluteEntfernung", 255);
+    // std::random_device dev;
+    // std::mt19937 rng(dev());
+    // std::uniform_int_distribution<std::mt19937::result_type> dist(0,2000000000);
+    //
+    // start("AbsoluteEntfernung", 255);
+    //
+    // set_speed(10);
+    // show_rotation(false);
+    //
+    //
+    //
+    // end();
 
-    set_speed(10);
-    show_rotation(false);
-    
-    int steps = 0;
+    Name name1 { "Michi" };
+    Name name2 { };
 
-    while(!stand_on_ankh())
-    {
-        if(abyss_in_front()) break;
-        
-        move();
-    }
+    name1 = name1;
 
-    if(!stand_on_ankh())
-    {
-        turn_around();
-
-        while(!stand_on_ankh())
-        {
-            if(abyss_in_front()) break;
-        
-            move();
-        }
-    }
-
-    while(!stand_on_terminal())
-    {
-        if(abyss_in_front()) break;
-        
-        move();
-        steps++;
-    }
-
-    if(!stand_on_terminal())
-    {
-        turn_around();
-        move(steps);
-        steps = 0;
-
-        while(!stand_on_terminal())
-        {
-            if(abyss_in_front()) break;
-        
-            move();
-            steps++;
-        }
-    }
-
-    set_terminal_value(steps);
-    turn_around();
-    move(steps);
-    
-    end();
+    std::cout << name1.get_name() << std::endl;
 }
+
